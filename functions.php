404bfc411a6392bfc1013acc25b28fb9dba19548
Mike Setzer <michaelsetzerucf@gmail.com>
2023-09-13 14:52:22 -0400

    Initial theme template cleanup

--- functions.php
+++ functions.php
@@ -1,11 +1,11 @@
 <?php
-namespace MyProject\Theme;
+namespace AFMainSite\Theme;
 
-define( 'MYPROJECT_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
+define( 'AFMAINSITE_THEME_DIR', trailingslashit( get_stylesheet_directory() ) );
 
 
 // Theme foundation
-include_once MYPROJECT_THEME_DIR . 'includes/config.php';
-include_once MYPROJECT_THEME_DIR . 'includes/meta.php';
+include_once AFMAINSITE_THEME_DIR . 'includes/config.php';
+include_once AFMAINSITE_THEME_DIR . 'includes/meta.php';
 
 // Add other includes to this file as needed.
--- includes/config.php
+++ includes/config.php
@@ -2,11 +2,11 @@
 /**
  * Handle all theme configuration here
  **/
-namespace MyProject\Theme\Includes\Config;
+namespace AFMainSite\Theme\Includes\Config;
 
 
-define( 'MYPROJECT_THEME_URL', get_stylesheet_directory_uri() );
-define( 'MYPROJECT_THEME_STATIC_URL', MYPROJECT_THEME_URL . '/static' );
-define( 'MYPROJECT_THEME_CSS_URL', MYPROJECT_THEME_STATIC_URL . '/css' );
-define( 'MYPROJECT_THEME_JS_URL', MYPROJECT_THEME_STATIC_URL . '/js' );
-define( 'MYPROJECT_THEME_IMG_URL', MYPROJECT_THEME_STATIC_URL . '/img' );
+define( 'AFMAINSITE_THEME_URL', get_stylesheet_directory_uri() );
+define( 'AFMAINSITE_THEME_STATIC_URL', AFMAINSITE_THEME_URL . '/static' );
+define( 'AFMAINSITE_THEME_CSS_URL', AFMAINSITE_THEME_STATIC_URL . '/css' );
+define( 'AFMAINSITE_THEME_JS_URL', AFMAINSITE_THEME_STATIC_URL . '/js' );
+define( 'AFMAINSITE_THEME_IMG_URL', AFMAINSITE_THEME_STATIC_URL . '/img' );
