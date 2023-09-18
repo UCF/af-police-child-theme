79737a4b9058ba7b7cf093f1b916a1df5e193dec
Mike Setzer <michaelsetzerucf@gmail.com>
2023-09-18 18:06:41 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@ function unit_block_init() {
 			'name'              => 'acf_unit_block',
 			'title'             => __('Unit block'),
 			'description'       => __('A custom block for displaying units on about us.'),
-			'render_template'   => get_template_directory() . '/template-unit-block.php',
+			//'render_template'   => get_template_directory() . '/template-unit-block.php',
 			'render_callback'   => 'my_custom_block_render_callback',
 			'category'          => 'formatting',
 			'icon'              => 'admin-comments',
@@ -51,9 +51,40 @@ function my_custom_block_render_callback( $block, $content = '', $is_preview = f
 			echo '<div style="border: 1px solid #000; padding: 16px; margin: 16px 0;">No unit fields set</div>';
 		endif;
 	} else {
-		// Pass the $post_id variable to your template
-		set_query_var('block_post_id', $post->ID);
-		include get_template_directory() . '/template-unit-block.php';
+		// Use global $post ID to fetch the repeater fields.
+		$post_id = $post->ID;
+
+		if( have_rows('unit', $post_id) ):
+			$accordion_id = "accordion_" . uniqid();
+			echo '<div id="'. esc_attr( $accordion_id ) .'" class="accordion">';
+
+			$index = 0;
+			while( have_rows('unit', $post_id) ) : the_row();
+				$unit_name = get_sub_field('unit_name');
+				$unit_description = get_sub_field('unit_description');
+				$heading_id = "heading_" . uniqid();
+				$collapse_id = "collapse_" . uniqid();
+
+				echo '<div class="card">';
+				echo '<div class="card-header" id="'. esc_attr( $heading_id ) .'">';
+				echo '<h5 class="mb-0">';
+				echo '<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#'. esc_attr( $collapse_id ) .'" aria-expanded="'. ($index === 0 ? 'true' : 'false') .'" aria-controls="'. esc_attr( $collapse_id ) .'">';
+				echo esc_html( $unit_name );
+				echo '</button>';
+				echo '</h5>';
+				echo '</div>';
+
+				echo '<div id="'. esc_attr( $collapse_id ) .'" class="collapse '. ($index === 0 ? 'show' : '') .'" aria-labelledby="'. esc_attr( $heading_id ) .'" data-parent="#'. esc_attr( $accordion_id ) .'">';
+				echo '<div class="card-body">';
+				echo wp_kses_post( $unit_description );
+				echo '</div>';
+				echo '</div>';
+				echo '</div>';
+
+				$index++;
+			endwhile;
+			echo '</div>';
+		endif;
 	}
 }
 
