4ebf2c706fca311bb266b70836920d773d50c194
Mike Setzer <michaelsetzerucf@gmail.com>
2023-09-18 17:04:04 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -21,6 +21,7 @@ function unit_block_init() {
 			'title'             => __('Unit block'),
 			'description'       => __('A custom block for displaying units on about us.'),
 			'render_template'   => get_template_directory() . '/template-unit-block.php',
+			'render_callback'   => 'my_custom_block_render_callback',
 			'category'          => 'formatting',
 			'icon'              => 'admin-comments',
 			'keywords'          => array( 'testimonial', 'quote' ),
@@ -30,3 +31,22 @@ function unit_block_init() {
 
 // Hook into setup.
 add_action('acf/init', 'unit_block_init');
+
+function my_custom_block_render_callback( $block ) {
+	// Get the ACF fields for this block
+	$unit_fields = get_field('unit');
+
+	// Check if we have any fields to display
+	if ($unit_fields) {
+		foreach ($unit_fields as $unit_field) {
+			// Display the unit name and description
+			echo '<div style="border: 1px solid #000; padding: 16px; margin: 16px 0;">';
+			echo '<h4>' . esc_html($unit_field['unit_name']) . '</h4>';
+			echo '<p>' . wp_kses_post($unit_field['unit_description']) . '</p>';
+			echo '</div>';
+		}
+	} else {
+		// Display a message indicating that no fields are set
+		echo '<div style="border: 1px solid #000; padding: 16px; margin: 16px 0;">No unit fields set</div>';
+	}
+}
