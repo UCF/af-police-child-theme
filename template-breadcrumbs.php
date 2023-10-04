048682c86d41ef635052f29e1083c21ddb7ebcf9
Mike Setzer <michaelsetzerucf@gmail.com>
2023-10-04 08:16:28 -0400

    Update template-breadcrumbs.php

--- template-breadcrumbs.php
+++ template-breadcrumbs.php
@@ -7,7 +7,7 @@
 <?php get_header(); the_post(); ?>
 
 	<article class="<?php echo $post->post_status; ?> post-list-item">
-		<div class="container-fluid mt-4 mt-sm-5 mb-5 pb-sm-4">
+		<div class="container-fluid mt-4 mt-sm-5">
 			<?php theme_breadcrumbs(); the_content(); ?>
 		</div>
 	</article>
