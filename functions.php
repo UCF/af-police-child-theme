b9282b1570b21cdd1146534224fe3e7e91c754b1
Mike Setzer <michaelsetzerucf@gmail.com>
2023-10-04 07:54:10 -0400

    Added breadcrumbs template

--- functions.php
+++ functions.php
@@ -90,3 +90,39 @@ function my_custom_block_render_callback( $block, $content = '', $is_preview = f
 
 // Hook into setup.
 add_action('acf/init', 'unit_block_init');
+
+//Fetch breadcrumbs to display at top of template page
+if ( ! function_exists( 'theme_breadcrumbs' ) ) {
+	function theme_breadcrumbs() {
+		global $post;
+
+		echo '<div class="container mt-4">';
+		echo '<ol class="breadcrumb" role="navigation" aria-label="breadcrumb">';
+
+		// Home link
+		echo '<li class="breadcrumb-item"><a href="' . home_url() . '">Home</a></li>';
+
+		// If it's a page and it has a parent
+		if (is_page() && $post->post_parent) {
+			$parent_id  = $post->post_parent;
+			$breadcrumbs = array();
+
+			while ($parent_id) {
+				$page          = get_page($parent_id);
+				$breadcrumbs[] = '<li class="breadcrumb-item"><a href="' . get_permalink($page->ID) . '">' . get_the_title($page->ID) . '</a></li>';
+				$parent_id     = $page->post_parent;
+			}
+
+			// Display the parent pages in correct order
+			echo implode(array_reverse($breadcrumbs));
+		}
+
+		// Current page or post
+		if (is_page() || is_single()) {
+			echo '<li class="breadcrumb-item active" aria-current="page">' . get_the_title() . '</li>';
+		}
+
+		echo '</ol>';
+		echo '</div>';
+	}
+}
---
+++ template-breadcrumbs.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: Breadcrumbs
+ * Template Post Type: page, post
+ */
+?>
+<?php get_header(); the_post(); ?>
+
+	<article class="<?php echo $post->post_status; ?> post-list-item">
+		<div class="container mt-4 mt-sm-5 mb-5 pb-sm-4">
+			<?php theme_breadcrumbs(); the_content(); ?>
+		</div>
+	</article>
+
+<?php get_footer(); ?>
