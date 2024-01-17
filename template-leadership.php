192b0c9aa32f1128a84ba1c62871355f5a8980de
Mike Setzer <michaelsetzerucf@gmail.com>
2024-01-17 13:34:41 -0500

    Update template-leadership.php

--- template-leadership.php
+++ template-leadership.php
@@ -18,12 +18,14 @@ the_post();
 					$args = array(
 						'post_type'      => 'leadership-post',
 						'posts_per_page' => -1, // Retrieve all posts
+						'orderby'        => 'meta_value_num', // Order by numeric meta value
+						'meta_key'       => 'order', // Field to sort by
+						'order'          => 'ASC', // Ascending order
 					);
 					$query = new WP_Query($args);
 
 					if ($query->have_posts()) :
 						while ($query->have_posts()) :
-							// The style you provided for displaying each leadership post
 							$query->the_post();
 							$post_id = get_the_ID();
 
@@ -33,14 +35,42 @@ the_post();
 							$person_title = get_field('title', $post_id);
 							$person_email = get_field('email', $post_id);
 							$person_phone = get_field('phone', $post_id);
-							$person_bio = get_field('bio', $post_id); // Changed from 'leadership_bio' to 'bio'
+							$person_bio = get_field('bio', $post_id);
 
 							// Unique ID for the collapse element
 							$collapse_id = 'person_bio_' . $post_id;
+							?>
 
-							// HTML structure for each person
-							include('path/to/your/person-display-template.php'); // You can separate the display logic into a different file for clarity
+							<div class="container pb-5">
+								<div class="row">
+									<div class="col col-4 col-md-3">
+										<?php if ($person_image): ?>
+											<img src="<?php echo esc_url($person_image['url']); ?>" alt="<?php echo esc_attr($person_image['alt']); ?>" class="img-thumbnail rounded-circle border-0" />
+										<?php endif; ?>
+									</div>
+									<div class="col col-8 col-md-9">
+										<h3><?php echo esc_html($person_name); ?></h3>
+										<p><?php echo esc_html($person_title); ?></p>
+										<?php if ($person_email): ?>
+											<fa class="fa fa-envelope mr-2"></fa> <a href="mailto:<?php echo esc_attr($person_email); ?>"><?php echo esc_html($person_email); ?></a><br>
+										<?php endif; ?>
+										<?php if ($person_phone): ?>
+											<fa class="fa fa-phone mr-2"></fa> <a href="tel:<?php echo esc_attr($person_phone); ?>"><?php echo esc_html($person_phone); ?></a><br>
+										<?php endif; ?>
 
+										<?php if ($person_bio): ?>
+											<!-- Button to toggle the bio -->
+											<a class="btn btn-primary btn-sm mt-3" data-toggle="collapse" href="#<?php echo $collapse_id; ?>" role="button" aria-expanded="false" aria-controls="<?php echo $collapse_id; ?>">Bio</a>
+
+											<!-- Collapsible container for the bio -->
+											<div class="collapse" id="<?php echo $collapse_id; ?>">
+												<div class="card card-body p-2 pt-3 border-0"><?php echo $person_bio; ?></div>
+											</div>
+										<?php endif; ?>
+									</div>
+								</div>
+							</div>
+						<?php
 						endwhile;
 						wp_reset_postdata();
 					endif;
@@ -48,9 +78,7 @@ the_post();
 				</div>
 			</div>
 
-			<?php
-			// Check if there is content before displaying the Main Content Area
-			if (!empty(get_the_content())) : ?>
+			<?php if (!empty(get_the_content())) : ?>
 				<!-- Main Content Area -->
 				<div class="row">
 					<div class="col-12">
